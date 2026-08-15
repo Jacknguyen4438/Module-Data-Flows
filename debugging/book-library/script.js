c9da8fd4034f6e05e586442304e64957afe8c279
Jacknguyen4438 <jacknguyen4438@gmail.com>
2026-08-15 04:24:47 +0100

    Corrected the read/unread display logic and fixed the broken delete button in the library table

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -28,11 +28,7 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (
-    title.value == "" ||
-    author.value == "" ||
-    pages.value == ""
-  ) {
+  if (title.value == "" || author.value == "" || pages.value == "") {
     alert("Please fill all fields!");
     return;
   }
@@ -41,7 +37,6 @@ function submit() {
   myLibrary.push(book);
   render();
 }
-
 
 function Book(title, author, pages, check) {
   this.title = title;
@@ -76,11 +71,12 @@ function render() {
     changeBut.className = "btn btn-success";
     wasReadCell.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    if (myLibrary[i].check == true) {
       readStatus = "Yes";
     } else {
       readStatus = "No";
     }
+
     changeBut.innerText = readStatus;
 
     changeBut.addEventListener("click", function () {
@@ -88,13 +84,14 @@ function render() {
       render();
     });
 
-    //add delete button to every row and render again
+    // add delete button to every row and render again
     let delButton = document.createElement("button");
-    delBut.id = i + 5;
-    deleteCell.appendChild(delBut);
-    delBut.className = "btn btn-warning";
-    delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    delButton.id = i + 5;
+    deleteCell.appendChild(delButton);
+    delButton.className = "btn btn-warning";
+    delButton.innerHTML = "Delete";
+
+    delButton.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
